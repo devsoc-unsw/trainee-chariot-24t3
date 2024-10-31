c06989ae33e7d0eb8bbb5b0470b9e47a28929a52
Jaden Tan <z5592415@ad.unsw.edu.au>
2024-10-31 21:49:45 +1100

    feat: added burger menu and sidebar

--- frontend/src/screens/MapPage.jsx
+++ frontend/src/screens/MapPage.jsx
@@ -1,29 +1,108 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import MenuIcon from '@mui/icons-material/Menu';
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import PlaceIcon from "@mui/icons-material/Place";
+import CreateIcon from "@mui/icons-material/Create";
+import TurnedInIcon from "@mui/icons-material/TurnedIn";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import CloseIcon from "@mui/icons-material/Close";
 
 function useMazeMap() {
+	const mapOptions = {
+		container: "map",
+		campuses: 111,
+		center: { lng: 151.231232432, lat: -33.917529664 },
+		zoom: 16.2,
+		zLevel: 1,
+	};
 
-  const mapOptions = {
-    container: 'map',
-    campuses: 111,
-    center: {lng: 151.231232432, lat:  -33.917529664 },
-    zoom: 16.2,
-    zLevel: 1,
-  }
+	const map = new window.Mazemap.Map(mapOptions);
+	map.addControl(new window.Mazemap.mapboxgl.NavigationControl());
+}
 
-  const map = new window.Mazemap.Map(mapOptions)
-  map.addControl(new window.Mazemap.mapboxgl.NavigationControl())
+function AnchorTemporaryDrawer() {
+	const [state, setState] = React.useState({
+		left: false,
+	});
 
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+		setState({ ...state, [anchor]: open });
+	};
+
+	const menuItems = [
+		{ text: "", icon: <CloseIcon /> },
+		{ text: "Create New Event", icon: <PlaceIcon /> },
+		{ text: "Edit Event", icon: <CreateIcon /> },
+		{ text: "Saved", icon: <TurnedInIcon /> },
+		{ text: "Today's Events", icon: <ListAltIcon /> },
+	];
+
+	const list = (anchor) => (
+		<Box
+			sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 300 }}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				{menuItems.map(({ text, icon }) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>{icon}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+		</Box>
+	);
+
+	return (
+		<div style={{ position: "absolute", top: 36, left: 5, zIndex: 10 }}>
+			<Button onClick={toggleDrawer("left", true)}>
+				<MenuIcon
+					style={{color: "black" }}
+				/>
+			</Button>
+			<Drawer
+				anchor="left"
+				open={state.left}
+				onClose={toggleDrawer("left", false)}
+			>
+				{list("left")}
+			</Drawer>
+		</div>
+	);
 }
 
 export default function MapPage() {
-  useEffect (() => {
-    useMazeMap()
-  }, [])
-  return (
-    <div className="flex h-screen">
-      <div className="relative flex-grow w-screen h-screen rounded-lg shadow-lg overflow-hidden">
-        <div id="map" className="absolute inset-0" />
-      </div>
-    </div>
-  )
+	useEffect(() => {
+		useMazeMap();
+	}, []);
+
+	return (
+		<div className="flex h-screen">
+			<AnchorTemporaryDrawer />
+			<div className="relative flex-grow w-screen h-screen rounded-lg shadow-lg overflow-hidden">
+				<div id="map" className="absolute inset-0" />
+			</div>
+		</div>
+	);
 }
