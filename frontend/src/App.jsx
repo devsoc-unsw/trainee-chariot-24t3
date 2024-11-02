85391d913651a6265d27e780c2f14d5e69774fa8
Giselle <cryponiess@gmail.com>
2024-11-02 19:51:48 +1100

    added frontend demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import MapPage from "./screens/MapPage";
 import EventList from "./screens/EventList";
 import EventPage from "./screens/EventPage";
+import Dummy from "./screens/Dummy";
 import "./App.css";
 
 function App() {
@@ -12,12 +13,14 @@ function App() {
         <a onClick={() => navigate("/")}>Map</a>
         <a onClick={() => navigate("/eventList")}>EventList</a>
         <a onClick={() => navigate("/eventPage")}>EventPage</a>
+        <a onClick={() => navigate("/dummy")}>Dummy</a>
       </div>
 
       <Routes>
         <Route path="/" element={<MapPage />} />
         <Route path="/eventList" element={<EventList />} />
         <Route path="/eventPage" element={<EventPage />} />
+        <Route path="/dummy" element={<Dummy />} />
       </Routes>
     </>
   );
