db98db7a4b70f1a7a045ba3c771d14ca17fd91f6
Jaden Tan <z5592415@ad.unsw.edu.au>
2024-12-09 23:23:26 +1100

    feat: added thumbnail url field and split time into start and end times

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -4,13 +4,15 @@ const eventSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     date: { type: Date, required: false },
-    time: { type: String, required: false },
+    startTime: { type: String, required: false },
+    endTime: { type: String, required: false },
     location: {
       coords: {type: Number, type: Number}, 
       building: { type: String },
       room: { type: String }
     },
     desc: {type: String},
+    imageUrl: {type: String}, 
   },
   { timestamps: true } // mongoose will store createdAt and updatedAt everytime
 );
--- frontend/src/screens/MapPage.jsx
+++ frontend/src/screens/MapPage.jsx
@@ -114,9 +114,11 @@ function AnchorTemporaryDrawer() {
 
   const [eventName, setEventName] = useState("")
   const [eventDate, setEventDate] = useState(null)
-  const [eventTime, setEventTime] = useState(null)
+  const [eventStart, setEventStart] = useState(null)
+  const [eventEnd, setEventEnd] = useState(null)
   const [eventLocation, setEventLocation] = useState({ lat: null, lng: null, building: "", room: "" });
   const [eventDesc, setEventDesc] = useState("")
+  const [thumbnailUrl, setThumbnailUrl] = useState("");
 
   const { searchInputRef, suggestionsRef } = useMazeMap(setEventLocation);
 
@@ -136,9 +138,11 @@ function AnchorTemporaryDrawer() {
     setOpenDialog(false)
     setEventName("")
     setEventDate(null)
-    setEventTime(null)
+    setEventStart(null)
+    setEventEnd(null)
     setEventLocation("")
     setEventDesc("")
+    setThumbnailUrl("")
   }
 
 const handleSubmitEvent = async () => {
@@ -151,9 +155,11 @@ const handleSubmitEvent = async () => {
   const eventData = {
     name: eventName,
     date: eventDate,
-    time: eventTime,
+    startTime: eventStart,
+    endTime: eventEnd,
     location: eventLocation,
     desc: eventDesc,
+    imageUrl: thumbnailUrl,
   };
 
   try {
@@ -248,9 +254,15 @@ const handleSubmitEvent = async () => {
                 renderInput={(params) => <TextField {...params} fullWidth sx={{ marginRight: 4 }}/>}
               />
               <TimePicker
-                label="Event Time"
-                value={eventTime}
-                onChange={(newValue) => setEventTime(newValue)}
+                label="Event Start"
+                value={eventStart}
+                onChange={(newValue) => setEventStart(newValue)}
+                renderInput={(params) => <TextField {...params} fullWidth />}
+              />
+              <TimePicker
+                label="Event End"
+                value={eventEnd}
+                onChange={(newValue) => setEventEnd(newValue)}
                 renderInput={(params) => <TextField {...params} fullWidth />}
               />
               </Box>
@@ -272,6 +284,13 @@ const handleSubmitEvent = async () => {
               label="Event Description"
               value={eventDesc}
               onChange={(e) => setEventDesc(e.target.value)}
+              autoComplete="off"
+            />
+            <TextField
+              fullWidth
+              label="Thumbnail URL"
+              value={thumbnailUrl}
+              onChange={(e) => setThumbnailUrl(e.target.value)}
               autoComplete="off"
             />
           </div>
