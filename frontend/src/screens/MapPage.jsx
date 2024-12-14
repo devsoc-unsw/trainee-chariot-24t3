a530bbe5e9e50be57adf2c5ef0400144ad1f30f6
Giselle <cryponiess@gmail.com>
2024-12-14 09:34:36 +0700

    merged from main

--- frontend/src/screens/MapPage.jsx
+++ frontend/src/screens/MapPage.jsx
@@ -1,35 +1,42 @@
-'use client'
+"use client";
 
-import { useEffect, useState, useRef } from "react"
-import * as React from "react"
-import Box from "@mui/material/Box"
-import Drawer from "@mui/material/Drawer"
-import Button from "@mui/material/Button"
-import List from "@mui/material/List"
-import Divider from "@mui/material/Divider"
-import MenuIcon from "@mui/icons-material/Menu"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
-import PlaceIcon from "@mui/icons-material/Place"
-import CreateIcon from "@mui/icons-material/Create"
-import TurnedInIcon from "@mui/icons-material/TurnedIn"
-import ListAltIcon from "@mui/icons-material/ListAlt"
-import CloseIcon from "@mui/icons-material/Close"
-import Dialog from "@mui/material/Dialog"
-import DialogActions from "@mui/material/DialogActions"
-import DialogContent from "@mui/material/DialogContent"
-import DialogTitle from "@mui/material/DialogTitle"
-import { TextField } from "@mui/material"
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker'
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState, useRef } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import PlaceIcon from "@mui/icons-material/Place";
+import CreateIcon from "@mui/icons-material/Create";
+import TurnedInIcon from "@mui/icons-material/TurnedIn";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import CloseIcon from "@mui/icons-material/Close";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { useNavigate } from "react-router-dom";
 
-function createMarker(coord, map, zLevel, color, eventName, eventLocation, eventTime) {
-  const lngLat = coord;
+function createMarker(
+  coord,
+  map,
+  zLevel,
+  color,
+  eventName,
+  eventLocation,
+  eventTime
+) {
   const marker = new Mazemap.MazeMarker({
     color: color,
     innerCircle: true,
@@ -43,13 +50,23 @@ function createMarker(coord, map, zLevel, color, eventName, eventLocation, event
 
   marker.on("click", () => {
     marker.remove();
-    createMarker(coord, map, zLevel, color);
+    createMarker(
+      coord,
+      map,
+      zLevel,
+      color,
+      eventName,
+      eventLocation,
+      eventTime
+    );
 
     new Mazemap.Popup({ closeOnClick: true, offset: [0, -6] })
       .setLngLat(marker.getLngLat())
-      .setHTML(`<h3 style="font-weight: bold">${eventName}</h3>
+      .setHTML(
+        `<h3 style="font-weight: bold">${eventName}</h3>
                 <p style="max-width: 160px;">${eventLocation}</p>
-                <p style="max-width: 160px;">${eventTime}</p>`)
+                <p style="max-width: 160px;">${eventTime}</p>`
+      )
       .addTo(map);
   });
 }
@@ -77,25 +94,24 @@ async function allOngoingEvents() {
 }
 
 function useMazeMap(setEventLocation) {
-  const mapRef = useRef(null)
-  const searchInputRef = useRef(null)
-  const suggestionsRef = useRef(null)
-  const searchInputInstanceRef = useRef(null)
+  const mapRef = useRef(null);
+  const searchInputRef = useRef(null);
+  const suggestionsRef = useRef(null);
+  const searchInputInstanceRef = useRef(null);
 
   useEffect(() => {
-    if (typeof window === 'undefined' || !window.Mazemap) return
+    if (typeof window === "undefined" || !window.Mazemap) return;
 
     const mapOptions = {
-      container: 'map',
+      container: "map",
       campuses: 111,
       center: { lng: 151.231232432, lat: -33.917529664 },
       zoom: 16.2,
       zLevel: 1,
-    }
+    };
 
     const map = new window.Mazemap.Map(mapOptions);
     map.addControl(new window.Mazemap.mapboxgl.NavigationControl());
-
 
     allOngoingEvents().then((data) => {
       data.forEach((item) => {
@@ -112,7 +128,6 @@ function useMazeMap(setEventLocation) {
       console.log(data);
     });
 
-
     // map.on('click', onMapClick);
 
     // function onMapClick(e){
@@ -121,7 +136,7 @@ function useMazeMap(setEventLocation) {
     //   createMarker(lngLat, map, zLevel, "#ff00cc", "DevSoc Event", "Blockhouse, G034", "");
     // }
 
-    mapRef.current = map
+    mapRef.current = map;
 
     const mySearch = new window.Mazemap.Search.SearchController({
       campusid: 111,
@@ -130,151 +145,166 @@ function useMazeMap(setEventLocation) {
       withbuilding: false,
       withtype: false,
       withcampus: false,
-      resultsFormat: 'geojson',
-    })
+      resultsFormat: "geojson",
+    });
 
     const initializeSearch = () => {
       if (searchInputRef.current && suggestionsRef.current) {
         const mySearchInput = new window.Mazemap.Search.SearchInput({
-          container: document.getElementById('search-input-container'),
+          container: document.getElementById("search-input-container"),
           input: searchInputRef.current,
           suggestions: suggestionsRef.current,
           searchController: mySearch,
-        })
+        });
 
-        mySearchInput.on("itemclick", function (e) { 
+        mySearchInput.on("itemclick", function (e) {
           const selectedResult = e.item;
           if (selectedResult) {
             const { geometry, properties } = selectedResult;
             setEventLocation({
               lat: geometry.coordinates[1],
               lng: geometry.coordinates[0],
-              building: properties.buildingname || '',
-              room: properties.title || '',
+              building: properties.buildingname || "",
+              room: properties.title || "",
             });
             console.log("hi");
           }
         });
 
-        searchInputInstanceRef.current = mySearchInput
+        searchInputInstanceRef.current = mySearchInput;
 
-        searchInputRef.current.addEventListener('input', () => {
+        searchInputRef.current.addEventListener("input", () => {
           if (searchInputInstanceRef.current) {
-            searchInputInstanceRef.current.trigger()
+            searchInputInstanceRef.current.trigger();
           }
-        })
+        });
       } else {
-        setTimeout(initializeSearch, 100)
+        setTimeout(initializeSearch, 100);
       }
-    }
+    };
 
-    initializeSearch()
+    initializeSearch();
 
     return () => {
-      map.remove()
-    }
-  }, [])
+      map.remove();
+    };
+  }, []);
 
-  return { searchInputRef, suggestionsRef }
+  return { searchInputRef, suggestionsRef };
 }
 
 function AnchorTemporaryDrawer() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [state, setState] = useState({
     left: false,
-  })
+  });
 
-  const [openDialog, setOpenDialog] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false);
 
-  const [eventName, setEventName] = useState("")
-  const [eventDate, setEventDate] = useState(null)
-  const [eventStart, setEventStart] = useState(null)
-  const [eventEnd, setEventEnd] = useState(null)
-  const [eventLocation, setEventLocation] = useState({ lat: null, lng: null, building: "", room: "" });
-  const [eventDesc, setEventDesc] = useState("")
+  const [eventName, setEventName] = useState("");
+  const [eventDate, setEventDate] = useState(null);
+  const [eventStart, setEventStart] = useState(null);
+  const [eventEnd, setEventEnd] = useState(null);
+  const [eventLocation, setEventLocation] = useState({
+    lat: null,
+    lng: null,
+    building: "",
+    room: "",
+  });
+  const [eventDesc, setEventDesc] = useState("");
   const [thumbnailUrl, setThumbnailUrl] = useState("");
 
   const { searchInputRef, suggestionsRef } = useMazeMap(setEventLocation);
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
     }
-    setState({ ...state, [anchor]: open })
-  }
-
-  const handleCreateEvent = () => {
-    setOpenDialog(true)
-    setState({ ...state, left: false })
-  }
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false)
-    setEventName("")
-    setEventDate(null)
-    setEventStart(null)
-    setEventEnd(null)
-    setEventLocation("")
-    setEventDesc("")
-    setThumbnailUrl("")
-  }
-
-const handleSubmitEvent = async () => {
-  if (!eventLocation.lat || !eventLocation.lng) {
-    console.log(eventLocation);
-    alert("Please select a valid location from the map search box.");
-    return;
-  }
-
-  const token = localStorage.getItem("token")  
-  const eventData = {
-    name: eventName,
-    date: eventDate,
-    startTime: eventStart,
-    endTime: eventEnd,
-    location: eventLocation,
-    token: token, 
-    desc: eventDesc,
-    imageUrl: thumbnailUrl,
+    setState({ ...state, [anchor]: open });
   };
 
-  try {
-    const response = await fetch("http://localhost:5050/event/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(eventData),
-    });
+  const handleCreateEvent = () => {
+    setOpenDialog(true);
+    setState({ ...state, left: false });
+  };
 
-    if (response.ok) {
-      alert("Event created successfully!");
-      handleCloseDialog();
-    } else {
-      consoles.log(response);
-      alert("Failed to create event. Please try again.");
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setEventName("");
+    setEventDate(null);
+    setEventStart(null);
+    setEventEnd(null);
+    setEventLocation("");
+    setEventDesc("");
+    setThumbnailUrl("");
+  };
+
+  const handleSubmitEvent = async () => {
+    if (!eventLocation.lat || !eventLocation.lng) {
+      console.log(eventLocation);
+      alert("Please select a valid location from the map search box.");
+      return;
     }
-  } catch (error) {
-    console.error("Error creating event:", error);
-    alert("An error occurred while creating the event. Please try again.");
-  }
-};
 
+    const token = localStorage.getItem("token");
+    const eventData = {
+      name: eventName,
+      date: eventDate,
+      startTime: eventStart,
+      endTime: eventEnd,
+      location: eventLocation,
+      token: token,
+      desc: eventDesc,
+      imageUrl: thumbnailUrl,
+    };
+
+    try {
+      const response = await fetch("http://localhost:5050/event/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(eventData),
+      });
+
+      if (response.ok) {
+        alert("Event created successfully!");
+        handleCloseDialog();
+      } else {
+        consoles.log(response);
+        alert("Failed to create event. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error creating event:", error);
+      alert("An error occurred while creating the event. Please try again.");
+    }
+  };
 
   const menuItems = [
-    { text: "", icon: <CloseIcon />, action: toggleDrawer('left', false) },
-    { text: "Create New Event", icon: <PlaceIcon />, action: handleCreateEvent },
+    { text: "", icon: <CloseIcon />, action: toggleDrawer("left", false) },
+    {
+      text: "Create New Event",
+      icon: <PlaceIcon />,
+      action: handleCreateEvent,
+    },
     { text: "Edit Event", icon: <CreateIcon />, action: () => {} },
     { text: "Saved", icon: <TurnedInIcon />, action: () => {} },
-    { text: "Event List", icon: <ListAltIcon />, action: () => {navigate('/eventList')} },
-  ]
-
+    {
+      text: "Event List",
+      icon: <ListAltIcon />,
+      action: () => {
+        navigate("/eventList");
+      },
+    },
+  ];
 
   const list = (anchor) => (
-
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 300 }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 300 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
@@ -291,29 +321,29 @@ const handleSubmitEvent = async () => {
       </List>
       <Divider />
     </Box>
-  )
+  );
 
   return (
     <div className="absolute top-auto left-auto z-10">
-      <Button onClick={toggleDrawer('left', true)}>
+      <Button onClick={toggleDrawer("left", true)}>
         <MenuIcon className="text-black" />
       </Button>
       <Drawer
         anchor="left"
         open={state.left}
-        onClose={toggleDrawer('left', false)}
+        onClose={toggleDrawer("left", false)}
       >
-        {list('left')}
+        {list("left")}
       </Drawer>
       <Dialog
         open={openDialog}
         onClose={handleCloseDialog}
-        sx={{ borderRadius: '16px' }}
+        sx={{ borderRadius: "16px" }}
       >
-        <DialogTitle style={{ backgroundColor: '#CFCFCF' }}>
+        <DialogTitle style={{ backgroundColor: "#CFCFCF" }}>
           Create New Event
         </DialogTitle>
-        <DialogContent style={{ backgroundColor: '#CFCFCF' }}>
+        <DialogContent style={{ backgroundColor: "#CFCFCF" }}>
           <div className="space-y-4 mt-4">
             <TextField
               fullWidth
@@ -323,28 +353,27 @@ const handleSubmitEvent = async () => {
               autoComplete="off"
             />
             <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Box 
-                display={'flex'}
-                justifyContent={'space-between'}
-                gap={2}
-              ><DatePicker
-                label="Event Date"
-                value={eventDate}
-                onChange={(newValue) => setEventDate(newValue)}
-                renderInput={(params) => <TextField {...params} fullWidth sx={{ marginRight: 4 }}/>}
-              />
-              <TimePicker
-                label="Event Start"
-                value={eventStart}
-                onChange={(newValue) => setEventStart(newValue)}
-                renderInput={(params) => <TextField {...params} fullWidth />}
-              />
-              <TimePicker
-                label="Event End"
-                value={eventEnd}
-                onChange={(newValue) => setEventEnd(newValue)}
-                renderInput={(params) => <TextField {...params} fullWidth />}
-              />
+              <Box display={"flex"} justifyContent={"space-between"} gap={2}>
+                <DatePicker
+                  label="Event Date"
+                  value={eventDate}
+                  onChange={(newValue) => setEventDate(newValue)}
+                  renderInput={(params) => (
+                    <TextField {...params} fullWidth sx={{ marginRight: 4 }} />
+                  )}
+                />
+                <TimePicker
+                  label="Event Start"
+                  value={eventStart}
+                  onChange={(newValue) => setEventStart(newValue)}
+                  renderInput={(params) => <TextField {...params} fullWidth />}
+                />
+                <TimePicker
+                  label="Event End"
+                  value={eventEnd}
+                  onChange={(newValue) => setEventEnd(newValue)}
+                  renderInput={(params) => <TextField {...params} fullWidth />}
+                />
               </Box>
             </LocalizationProvider>
             <div id="search-input-container" className="search-control-default">
@@ -354,7 +383,11 @@ const handleSubmitEvent = async () => {
                 inputRef={searchInputRef}
                 autoComplete="off"
               />
-              <div ref={suggestionsRef} id="suggestions" className="search-suggestions default"></div>
+              <div
+                ref={suggestionsRef}
+                id="suggestions"
+                className="search-suggestions default"
+              ></div>
             </div>
             <TextField
               id="outlined-multiline-static"
@@ -375,13 +408,13 @@ const handleSubmitEvent = async () => {
             />
           </div>
         </DialogContent>
-        <DialogActions style={{ backgroundColor: '#CFCFCF' }}>
+        <DialogActions style={{ backgroundColor: "#CFCFCF" }}>
           <Button onClick={handleCloseDialog}>Cancel</Button>
           <Button onClick={handleSubmitEvent}>Create</Button>
         </DialogActions>
       </Dialog>
     </div>
-  )
+  );
 }
 
 export default function MapPage() {
@@ -392,6 +425,5 @@ export default function MapPage() {
         <div id="map" className="absolute inset-0" />
       </div>
     </div>
-  )
+  );
 }
-
